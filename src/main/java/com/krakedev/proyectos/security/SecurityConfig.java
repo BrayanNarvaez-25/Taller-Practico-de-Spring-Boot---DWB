2aed48aa3ddf9db6149b09bf8d2065c09da6b09e
BrayanNarvaez-25 <brayann251107g@gmail.com>
2026-06-02 20:52:26 -0500

    BN: Se crea configuracion de seguridad

---
+++ src/main/java/com/krakedev/proyectos/security/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.krakedev.proyectos.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthenticationFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(
+            HttpSecurity http) throws Exception {
+        http
+          .csrf(csrf -> csrf.disable())
+          .sessionManagement(sess -> sess
+            .sessionCreationPolicy(
+                SessionCreationPolicy.STATELESS))
+          .authorizeHttpRequests(auth -> auth
+            .requestMatchers(
+                "/api/auth/registrar",
+                "/api/auth/login")
+            .permitAll()
+            .anyRequest()
+            .authenticated())
+          .addFilterBefore(jwtFilter,
+            UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
