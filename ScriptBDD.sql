3ada8efc75c4568ac6c23637ebb2d852dfb33757
BrayanNarvaez-25 <brayann251107g@gmail.com>
2026-06-02 20:30:10 -0500

    BN: Nueva dependencia y nueva tabla

---
+++ ScriptBDD.sql
@@ -0,0 +1,12 @@
+SELECT * FROM proyectos;
+SELECT * FROM empleados;
+SELECT * FROM tareas;
+
+CREATE TABLE usuarios (
+  id       SERIAL PRIMARY KEY,
+  username VARCHAR(50)  NOT NULL UNIQUE,
+  password VARCHAR(100) NOT NULL,
+  rol      VARCHAR(20)  NOT NULL
+);
+
+SELECT * FROM usuarios;
